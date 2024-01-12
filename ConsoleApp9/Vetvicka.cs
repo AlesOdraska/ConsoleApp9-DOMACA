4c4406bd857c98eaf1a17ac12147cacedd593aa0
AlesOdraska <151876339+AlesOdraska@users.noreply.github.com>
2024-01-12 12:27:51 +0100

    POPIS

---
+++ ConsoleApp9/Vetvicka.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Vetvicka : Strom
+    {
+        public Vetvicka(int radius1, int vyska1) : base(radius1, vyska1)
+        {
+            radius = radius1;
+            vyska = vyska1;
+        }
+
+    }
+        
+   
+}
